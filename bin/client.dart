3de7128b5b8ba2e8bca41718ed1196bf6bd70921
David J. H. Shih <djh.shih@gmail.com>
2019-02-11 00:10:16 -0600

    Added client and server entry points.

---
+++ bin/client.dart
@@ -0,0 +1,5 @@
+import 'package:werewolf/src/client.dart';
+
+main(List<String> args) async {
+  await new Client().main(args);
+}
---
+++ bin/server.dart
@@ -0,0 +1,5 @@
+import 'package:werewolf/src/server.dart';
+
+main(List<String> args) async {
+  await new Server().main(args);
+}
